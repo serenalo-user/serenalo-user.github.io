0b4212edfeca702500fca376e742d41d89032174
serenalo-user <serena.lo.81518@gmail.com>
2024-12-13 11:06:05 +0800

    Add files via upload

---
+++ code7.js
@@ -0,0 +1,233 @@
+gdjs._27792_28020_20083_35299_35598Code = {};
+gdjs._27792_28020_20083_35299_35598Code.GDNewSpriteObjects1= [];
+gdjs._27792_28020_20083_35299_35598Code.GDNewSpriteObjects2= [];
+gdjs._27792_28020_20083_35299_35598Code.GDNewSprite6Objects1= [];
+gdjs._27792_28020_20083_35299_35598Code.GDNewSprite6Objects2= [];
+gdjs._27792_28020_20083_35299_35598Code.GDNewSprite7Objects1= [];
+gdjs._27792_28020_20083_35299_35598Code.GDNewSprite7Objects2= [];
+gdjs._27792_28020_20083_35299_35598Code.GDNewSprite2Objects1= [];
+gdjs._27792_28020_20083_35299_35598Code.GDNewSprite2Objects2= [];
+gdjs._27792_28020_20083_35299_35598Code.GDEObjects1= [];
+gdjs._27792_28020_20083_35299_35598Code.GDEObjects2= [];
+gdjs._27792_28020_20083_35299_35598Code.GDLObjects1= [];
+gdjs._27792_28020_20083_35299_35598Code.GDLObjects2= [];
+gdjs._27792_28020_20083_35299_35598Code.GDPObjects1= [];
+gdjs._27792_28020_20083_35299_35598Code.GDPObjects2= [];
+gdjs._27792_28020_20083_35299_35598Code.GDHObjects1= [];
+gdjs._27792_28020_20083_35299_35598Code.GDHObjects2= [];
+gdjs._27792_28020_20083_35299_35598Code.GDNewSprite4Objects1= [];
+gdjs._27792_28020_20083_35299_35598Code.GDNewSprite4Objects2= [];
+gdjs._27792_28020_20083_35299_35598Code.GDNewSprite5Objects1= [];
+gdjs._27792_28020_20083_35299_35598Code.GDNewSprite5Objects2= [];
+gdjs._27792_28020_20083_35299_35598Code.GDNewSprite8Objects1= [];
+gdjs._27792_28020_20083_35299_35598Code.GDNewSprite8Objects2= [];
+gdjs._27792_28020_20083_35299_35598Code.GDH2Objects1= [];
+gdjs._27792_28020_20083_35299_35598Code.GDH2Objects2= [];
+gdjs._27792_28020_20083_35299_35598Code.GDE2Objects1= [];
+gdjs._27792_28020_20083_35299_35598Code.GDE2Objects2= [];
+gdjs._27792_28020_20083_35299_35598Code.GDL2Objects1= [];
+gdjs._27792_28020_20083_35299_35598Code.GDL2Objects2= [];
+gdjs._27792_28020_20083_35299_35598Code.GDP2Objects1= [];
+gdjs._27792_28020_20083_35299_35598Code.GDP2Objects2= [];
+gdjs._27792_28020_20083_35299_35598Code.GDNewText2Objects1= [];
+gdjs._27792_28020_20083_35299_35598Code.GDNewText2Objects2= [];
+gdjs._27792_28020_20083_35299_35598Code.GDNewTextObjects1= [];
+gdjs._27792_28020_20083_35299_35598Code.GDNewTextObjects2= [];
+gdjs._27792_28020_20083_35299_35598Code.GDNewSprite3Objects1= [];
+gdjs._27792_28020_20083_35299_35598Code.GDNewSprite3Objects2= [];
+gdjs._27792_28020_20083_35299_35598Code.GD_9523565_9535441_9526684Objects1= [];
+gdjs._27792_28020_20083_35299_35598Code.GD_9523565_9535441_9526684Objects2= [];
+
+
+gdjs._27792_28020_20083_35299_35598Code.mapOfGDgdjs_9546_959527792_959528020_959520083_959535299_959535598Code_9546GDHObjects1Objects = Hashtable.newFrom({"H": gdjs._27792_28020_20083_35299_35598Code.GDHObjects1});
+gdjs._27792_28020_20083_35299_35598Code.mapOfGDgdjs_9546_959527792_959528020_959520083_959535299_959535598Code_9546GDH2Objects1Objects = Hashtable.newFrom({"H2": gdjs._27792_28020_20083_35299_35598Code.GDH2Objects1});
+gdjs._27792_28020_20083_35299_35598Code.mapOfGDgdjs_9546_959527792_959528020_959520083_959535299_959535598Code_9546GDEObjects1Objects = Hashtable.newFrom({"E": gdjs._27792_28020_20083_35299_35598Code.GDEObjects1});
+gdjs._27792_28020_20083_35299_35598Code.mapOfGDgdjs_9546_959527792_959528020_959520083_959535299_959535598Code_9546GDE2Objects1Objects = Hashtable.newFrom({"E2": gdjs._27792_28020_20083_35299_35598Code.GDE2Objects1});
+gdjs._27792_28020_20083_35299_35598Code.mapOfGDgdjs_9546_959527792_959528020_959520083_959535299_959535598Code_9546GDLObjects1Objects = Hashtable.newFrom({"L": gdjs._27792_28020_20083_35299_35598Code.GDLObjects1});
+gdjs._27792_28020_20083_35299_35598Code.mapOfGDgdjs_9546_959527792_959528020_959520083_959535299_959535598Code_9546GDL2Objects1Objects = Hashtable.newFrom({"L2": gdjs._27792_28020_20083_35299_35598Code.GDL2Objects1});
+gdjs._27792_28020_20083_35299_35598Code.mapOfGDgdjs_9546_959527792_959528020_959520083_959535299_959535598Code_9546GDPObjects1Objects = Hashtable.newFrom({"P": gdjs._27792_28020_20083_35299_35598Code.GDPObjects1});
+gdjs._27792_28020_20083_35299_35598Code.mapOfGDgdjs_9546_959527792_959528020_959520083_959535299_959535598Code_9546GDP2Objects1Objects = Hashtable.newFrom({"P2": gdjs._27792_28020_20083_35299_35598Code.GDP2Objects1});
+gdjs._27792_28020_20083_35299_35598Code.asyncCallback11136060 = function (runtimeScene, asyncObjectsList) {
+{gdjs.evtTools.runtimeScene.replaceScene(runtimeScene, "浴室2", false);
+}{gdjs.evtTools.sound.playSound(runtimeScene, "Ghost.mp3", true, 20, 1);
+}}
+gdjs._27792_28020_20083_35299_35598Code.eventsList0 = function(runtimeScene) {
+
+{
+
+
+{
+{
+const asyncObjectsList = new gdjs.LongLivedObjectsList();
+runtimeScene.getAsyncTasksManager().addTask(gdjs.evtTools.runtimeScene.wait(1), (runtimeScene) => (gdjs._27792_28020_20083_35299_35598Code.asyncCallback11136060(runtimeScene, asyncObjectsList)));
+}
+}
+
+}
+
+
+};gdjs._27792_28020_20083_35299_35598Code.eventsList1 = function(runtimeScene) {
+
+{
+
+gdjs.copyArray(runtimeScene.getObjects("P"), gdjs._27792_28020_20083_35299_35598Code.GDPObjects1);
+gdjs.copyArray(runtimeScene.getObjects("P2"), gdjs._27792_28020_20083_35299_35598Code.GDP2Objects1);
+
+let isConditionTrue_0 = false;
+isConditionTrue_0 = false;
+isConditionTrue_0 = gdjs.evtTools.object.hitBoxesCollisionTest(gdjs._27792_28020_20083_35299_35598Code.mapOfGDgdjs_9546_959527792_959528020_959520083_959535299_959535598Code_9546GDPObjects1Objects, gdjs._27792_28020_20083_35299_35598Code.mapOfGDgdjs_9546_959527792_959528020_959520083_959535299_959535598Code_9546GDP2Objects1Objects, false, runtimeScene, false);
+if (isConditionTrue_0) {
+{runtimeScene.getGame().getVariables().getFromIndex(4).setNumber(1);
+}
+{ //Subevents
+gdjs._27792_28020_20083_35299_35598Code.eventsList0(runtimeScene);} //End of subevents
+}
+
+}
+
+
+};gdjs._27792_28020_20083_35299_35598Code.eventsList2 = function(runtimeScene) {
+
+{
+
+gdjs.copyArray(runtimeScene.getObjects("L"), gdjs._27792_28020_20083_35299_35598Code.GDLObjects1);
+gdjs.copyArray(runtimeScene.getObjects("L2"), gdjs._27792_28020_20083_35299_35598Code.GDL2Objects1);
+
+let isConditionTrue_0 = false;
+isConditionTrue_0 = false;
+isConditionTrue_0 = gdjs.evtTools.object.hitBoxesCollisionTest(gdjs._27792_28020_20083_35299_35598Code.mapOfGDgdjs_9546_959527792_959528020_959520083_959535299_959535598Code_9546GDLObjects1Objects, gdjs._27792_28020_20083_35299_35598Code.mapOfGDgdjs_9546_959527792_959528020_959520083_959535299_959535598Code_9546GDL2Objects1Objects, false, runtimeScene, false);
+if (isConditionTrue_0) {
+
+{ //Subevents
+gdjs._27792_28020_20083_35299_35598Code.eventsList1(runtimeScene);} //End of subevents
+}
+
+}
+
+
+};gdjs._27792_28020_20083_35299_35598Code.eventsList3 = function(runtimeScene) {
+
+{
+
+gdjs.copyArray(runtimeScene.getObjects("E"), gdjs._27792_28020_20083_35299_35598Code.GDEObjects1);
+gdjs.copyArray(runtimeScene.getObjects("E2"), gdjs._27792_28020_20083_35299_35598Code.GDE2Objects1);
+
+let isConditionTrue_0 = false;
+isConditionTrue_0 = false;
+isConditionTrue_0 = gdjs.evtTools.object.hitBoxesCollisionTest(gdjs._27792_28020_20083_35299_35598Code.mapOfGDgdjs_9546_959527792_959528020_959520083_959535299_959535598Code_9546GDEObjects1Objects, gdjs._27792_28020_20083_35299_35598Code.mapOfGDgdjs_9546_959527792_959528020_959520083_959535299_959535598Code_9546GDE2Objects1Objects, false, runtimeScene, false);
+if (isConditionTrue_0) {
+
+{ //Subevents
+gdjs._27792_28020_20083_35299_35598Code.eventsList2(runtimeScene);} //End of subevents
+}
+
+}
+
+
+};gdjs._27792_28020_20083_35299_35598Code.mapOfGDgdjs_9546_959527792_959528020_959520083_959535299_959535598Code_9546GDNewSprite5Objects1Objects = Hashtable.newFrom({"NewSprite5": gdjs._27792_28020_20083_35299_35598Code.GDNewSprite5Objects1});
+gdjs._27792_28020_20083_35299_35598Code.eventsList4 = function(runtimeScene) {
+
+{
+
+
+let isConditionTrue_0 = false;
+isConditionTrue_0 = false;
+isConditionTrue_0 = gdjs.evtTools.runtimeScene.sceneJustBegins(runtimeScene);
+if (isConditionTrue_0) {
+{gdjs.evtTools.runtimeScene.createObjectsFromExternalLayout(runtimeScene, "額外物件", 0, 0, 0);
+}{gdjs.evtTools.camera.hideLayer(runtimeScene, "對話框");
+}{gdjs.evtTools.camera.hideLayer(runtimeScene, "點擊");
+}{gdjs.evtTools.sound.playSound(runtimeScene, "Ghost.mp3", true, 20, 1);
+}}
+
+}
+
+
+{
+
+gdjs.copyArray(runtimeScene.getObjects("H"), gdjs._27792_28020_20083_35299_35598Code.GDHObjects1);
+gdjs.copyArray(runtimeScene.getObjects("H2"), gdjs._27792_28020_20083_35299_35598Code.GDH2Objects1);
+
+let isConditionTrue_0 = false;
+isConditionTrue_0 = false;
+isConditionTrue_0 = gdjs.evtTools.object.hitBoxesCollisionTest(gdjs._27792_28020_20083_35299_35598Code.mapOfGDgdjs_9546_959527792_959528020_959520083_959535299_959535598Code_9546GDHObjects1Objects, gdjs._27792_28020_20083_35299_35598Code.mapOfGDgdjs_9546_959527792_959528020_959520083_959535299_959535598Code_9546GDH2Objects1Objects, false, runtimeScene, true);
+if (isConditionTrue_0) {
+
+{ //Subevents
+gdjs._27792_28020_20083_35299_35598Code.eventsList3(runtimeScene);} //End of subevents
+}
+
+}
+
+
+{
+
+gdjs.copyArray(runtimeScene.getObjects("NewSprite5"), gdjs._27792_28020_20083_35299_35598Code.GDNewSprite5Objects1);
+
+let isConditionTrue_0 = false;
+isConditionTrue_0 = false;
+isConditionTrue_0 = gdjs.evtTools.input.cursorOnObject(gdjs._27792_28020_20083_35299_35598Code.mapOfGDgdjs_9546_959527792_959528020_959520083_959535299_959535598Code_9546GDNewSprite5Objects1Objects, runtimeScene, true, false);
+if (isConditionTrue_0) {
+isConditionTrue_0 = false;
+isConditionTrue_0 = gdjs.evtTools.input.isMouseButtonReleased(runtimeScene, "Left");
+}
+if (isConditionTrue_0) {
+{gdjs.evtTools.sound.playSound(runtimeScene, "Cancel1.ogg", false, 20, 1);
+}{gdjs.evtTools.runtimeScene.replaceScene(runtimeScene, "浴室2", false);
+}{gdjs.evtTools.sound.playSound(runtimeScene, "Ghost.mp3", true, 20, 1);
+}}
+
+}
+
+
+};
+
+gdjs._27792_28020_20083_35299_35598Code.func = function(runtimeScene) {
+runtimeScene.getOnceTriggers().startNewFrame();
+
+gdjs._27792_28020_20083_35299_35598Code.GDNewSpriteObjects1.length = 0;
+gdjs._27792_28020_20083_35299_35598Code.GDNewSpriteObjects2.length = 0;
+gdjs._27792_28020_20083_35299_35598Code.GDNewSprite6Objects1.length = 0;
+gdjs._27792_28020_20083_35299_35598Code.GDNewSprite6Objects2.length = 0;
+gdjs._27792_28020_20083_35299_35598Code.GDNewSprite7Objects1.length = 0;
+gdjs._27792_28020_20083_35299_35598Code.GDNewSprite7Objects2.length = 0;
+gdjs._27792_28020_20083_35299_35598Code.GDNewSprite2Objects1.length = 0;
+gdjs._27792_28020_20083_35299_35598Code.GDNewSprite2Objects2.length = 0;
+gdjs._27792_28020_20083_35299_35598Code.GDEObjects1.length = 0;
+gdjs._27792_28020_20083_35299_35598Code.GDEObjects2.length = 0;
+gdjs._27792_28020_20083_35299_35598Code.GDLObjects1.length = 0;
+gdjs._27792_28020_20083_35299_35598Code.GDLObjects2.length = 0;
+gdjs._27792_28020_20083_35299_35598Code.GDPObjects1.length = 0;
+gdjs._27792_28020_20083_35299_35598Code.GDPObjects2.length = 0;
+gdjs._27792_28020_20083_35299_35598Code.GDHObjects1.length = 0;
+gdjs._27792_28020_20083_35299_35598Code.GDHObjects2.length = 0;
+gdjs._27792_28020_20083_35299_35598Code.GDNewSprite4Objects1.length = 0;
+gdjs._27792_28020_20083_35299_35598Code.GDNewSprite4Objects2.length = 0;
+gdjs._27792_28020_20083_35299_35598Code.GDNewSprite5Objects1.length = 0;
+gdjs._27792_28020_20083_35299_35598Code.GDNewSprite5Objects2.length = 0;
+gdjs._27792_28020_20083_35299_35598Code.GDNewSprite8Objects1.length = 0;
+gdjs._27792_28020_20083_35299_35598Code.GDNewSprite8Objects2.length = 0;
+gdjs._27792_28020_20083_35299_35598Code.GDH2Objects1.length = 0;
+gdjs._27792_28020_20083_35299_35598Code.GDH2Objects2.length = 0;
+gdjs._27792_28020_20083_35299_35598Code.GDE2Objects1.length = 0;
+gdjs._27792_28020_20083_35299_35598Code.GDE2Objects2.length = 0;
+gdjs._27792_28020_20083_35299_35598Code.GDL2Objects1.length = 0;
+gdjs._27792_28020_20083_35299_35598Code.GDL2Objects2.length = 0;
+gdjs._27792_28020_20083_35299_35598Code.GDP2Objects1.length = 0;
+gdjs._27792_28020_20083_35299_35598Code.GDP2Objects2.length = 0;
+gdjs._27792_28020_20083_35299_35598Code.GDNewText2Objects1.length = 0;
+gdjs._27792_28020_20083_35299_35598Code.GDNewText2Objects2.length = 0;
+gdjs._27792_28020_20083_35299_35598Code.GDNewTextObjects1.length = 0;
+gdjs._27792_28020_20083_35299_35598Code.GDNewTextObjects2.length = 0;
+gdjs._27792_28020_20083_35299_35598Code.GDNewSprite3Objects1.length = 0;
+gdjs._27792_28020_20083_35299_35598Code.GDNewSprite3Objects2.length = 0;
+gdjs._27792_28020_20083_35299_35598Code.GD_9523565_9535441_9526684Objects1.length = 0;
+gdjs._27792_28020_20083_35299_35598Code.GD_9523565_9535441_9526684Objects2.length = 0;
+
+gdjs._27792_28020_20083_35299_35598Code.eventsList4(runtimeScene);
+
+return;
+
+}
+
+gdjs['_27792_28020_20083_35299_35598Code'] = gdjs._27792_28020_20083_35299_35598Code;
